3ffe18da47d516202caba2219f9567fc358ec898
Abdelrahman <boudysallam1@gmail.com>
2025-10-28 20:04:10 +0300

    feat: initialize Spring Boot app and add HelloController test endpoint

---
+++ src/main/java/com/university/backend/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.university.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot is running!";
+    }
+}

f42e223f1913ebbeb34f4d63d61e5d77ec8e5c52
Abdelrahman <boudysallam1@gmail.com>
2025-11-19 01:09:40 +0200

    fix: student entity bug

--- backend/src/main/java/com/university/backend/model/ExamResult.java
+++ backend/src/main/java/com/university/backend/model/ExamResult.java
@@ -1,29 +1,29 @@
-//package com.university.backend.model;
-//
-//import jakarta.persistence.*;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "exam_result")
-//public class ExamResult {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "exam_result_id")
-//    private int assignmentId;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "exam_id", nullable = false)
-//    private Exam course_id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "student_id", nullable = false)
-//    private Student student_id;
-//
-//    @Column(name = "score")
-//    private int score;
-//
-//    @Column(name = "feedback", columnDefinition = "TEXT")
-//    private String feedback;
-//}
+package com.university.backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "exam_result")
+public class ExamResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exam_result_id")
+    private int assignmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exam_id", nullable = false)
+    private Exam course_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student_id;
+
+    @Column(name = "score")
+    private int score;
+
+    @Column(name = "feedback", columnDefinition = "TEXT")
+    private String feedback;
+}

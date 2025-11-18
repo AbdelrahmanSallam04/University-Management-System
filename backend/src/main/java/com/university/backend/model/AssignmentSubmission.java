53ddfba51674ca91a54b2f399c43637fe9cd03b6
Abdelrahman <boudysallam1@gmail.com>
2025-11-19 00:42:38 +0200

    feat: connected the database

--- backend/src/main/java/com/university/backend/model/AssignmentSubmission.java
+++ backend/src/main/java/com/university/backend/model/AssignmentSubmission.java
@@ -1,34 +1,34 @@
-package com.university.backend.model;
-
-import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "assignment_submission")
-public class AssignmentSubmission {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "assignment_submission_id")
-    private int assignment_submission_id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "assignment_id", nullable = false)
-    private Assignment assignment_id;
-
-    @Column(name = "student_id", nullable = false, length = 255)
-    private Student student_id;
-
-    @Column(name = "submitted_at", nullable = false)
-    private LocalDateTime submitted_at;
-
-    @Column(name = "answer", columnDefinition = "TEXT")
-    private String answer;
-
-    @Column(name = "grade")
-    private int grade;
-
-    @Column(name = "feedback", columnDefinition = "TEXT")
-    private String feedback;
-}
+//package com.university.backend.model;
+//
+//import jakarta.persistence.*;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "assignment_submission")
+//public class AssignmentSubmission {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "assignment_submission_id")
+//    private int assignment_submission_id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "assignment_id", nullable = false)
+//    private Assignment assignment_id;
+//
+//    @Column(name = "student_id", nullable = false, length = 255)
+//    private Student student_id;
+//
+//    @Column(name = "submitted_at", nullable = false)
+//    private LocalDateTime submitted_at;
+//
+//    @Column(name = "answer", columnDefinition = "TEXT")
+//    private String answer;
+//
+//    @Column(name = "grade")
+//    private int grade;
+//
+//    @Column(name = "feedback", columnDefinition = "TEXT")
+//    private String feedback;
+//}

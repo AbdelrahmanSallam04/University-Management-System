902dd3eadf7c3b61198f2b7ae98736aea29ae772
Abdelrahman <boudysallam1@gmail.com>
2025-12-09 03:14:27 +0200

    fix: fixed wronge directory in the imports

--- frontend/src/components/CreateAccount.jsx
+++ frontend/src/components/CreateAccount.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../../styles/CreateAccount.css';
+import '../styles/CreateAccount.css';
 
 const CreateAccount = () => {
   const [formData, setFormData] = useState({

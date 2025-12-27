55454b69f94b9ee44d421b7539be140b1f9b99d9
Abdelrahman <boudysallam1@gmail.com>
2025-12-27 02:15:13 +0200

    refactor: deleted unnecessary imports

--- frontend/src/components/MaintenanceReportForm.jsx
+++ frontend/src/components/MaintenanceReportForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchAvailableRooms, fetchAllRooms } from '../services/roomService';
+import { fetchAllRooms } from '../services/roomService';
 import {submitReport} from '../services/maintenanceService';
 import '../styles/MaintenanceReportForm.css';
 
